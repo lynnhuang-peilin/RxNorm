b291cfb6fec94edae9c10c33bee425cc5a00975c
lynnhuang-peilin <lynnhuang.peilin@gmail.com>
2023-05-20 19:31:33 -0400

    Shiny Template

---
+++ RxNorm Shiny.R
@@ -0,0 +1,100 @@
+# Overview ###################################
+# title: "RxNorm Shiny app"
+# author: "Lynn Huang"
+# date: "2023-May-20"
+# output: 
+###
+
+
+# Setup ###################################
+getwd()
+# First specify the packages of interest
+packages = c("tidyverse","jsonlite", "readxl","readr","curl","stringr",
+             "bit64","sqldf", "haven","httr","openxlsx","shiny")
+# Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
+#上传数据ui
+ui_upload <- sidebarLayout(
+  sidebarPanel(
+    fileInput("file", "Data", buttonLabel = "Upload..."),
+    textInput("delim", "Delimiter (leave blank to guess)", ""),
+    numericInput("skip", "Rows to skip", 0, min = 0),
+    numericInput("rows", "Rows to preview", 10, min = 1)
+  ),
+  mainPanel(
+    h3("Raw data"),
+    tableOutput("preview1")
+  )
+)
+
+##处理数据ui
+ui_clean <- sidebarLayout(
+  sidebarPanel(
+    checkboxInput("snake", "Rename columns to snake case?"),
+    checkboxInput("constant", "Remove constant columns?"),
+    checkboxInput("empty", "Remove empty cols?")
+  ),
+  mainPanel(
+    h3("Cleaner data"),
+    tableOutput("preview2")
+  )
+)
+
+##下载数据ui
+ui_download <- fluidRow(
+  column(width = 12, downloadButton("download", class = "btn-block"))
+)
+ui <- fluidPage(
+  ui_upload,
+  ui_clean,
+  ui_download
+)
+server <- function(input, output, session) {
+  # Upload ---------------------------------------------------------------
+  raw <- reactive({
+    req(input$file)
+    delim <- if (input$delim == "") NULL else input$delim
+    vroom::vroom(input$file$datapath, delim = delim, skip = input$skip)
+  })
+  output$preview1 <- renderTable(head(raw(), input$rows))
+  
+  # Clean ----------------------------------------------------------------
+  tidied <- reactive({
+    out <- raw()
+    if (input$snake) {
+      names(out) <- janitor::make_clean_names(names(out))
+    }
+    if (input$empty) {
+      out <- janitor::remove_empty(out, "cols")
+    }
+    if (input$constant) {
+      out <- janitor::remove_constant(out)
+    }
+    
+    out
+  })
+  output$preview2 <- renderTable(head(tidied(), input$rows))
+  
+  # Download -------------------------------------------------------------
+  output$download <- downloadHandler(
+    filename = function() {
+      paste0(tools::file_path_sans_ext(input$file$name), ".tsv")
+    },
+    content = function(file) {
+      vroom::vroom_write(tidied(), file)
+    }
+  )
+}
+
+shinyApp(ui, server)
+
